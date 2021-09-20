05f0c8b01e9b74c3a57c8fcb0cc1a85ae34d64ce
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-09-20 12:53:05 +0530

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -28,14 +28,14 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.http import get
 
 __MODULE__ = "Repo"
-__HELP__ = "/repo - To Get My Github Repository Link " "And Support Group Link"
+__HELP__ = "/repo - Get My Github Repository Link " "And Support Group Link"
 
 
 @app.on_message(filters.command("repo") & ~filters.edited)
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
+        " https://t.me/zyruz_devs"
     )
     list_of_users = ""
     count = 1
@@ -45,7 +45,7 @@ async def repo(_, message):
         )
         count += 1
 
-    text = f"""[Github](https://github.com/thehamkercat/WilliamButcherBot) | [Group](t.me/PatheticProgrammers)
+    text = f"""[REPO](https://t.me/tubots) | [Group](t.me/unitedbotsupport)
 ```----------------
 | Contributors |
 ----------------```
